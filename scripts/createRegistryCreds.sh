ab7b6a0caa4375fd241bb1c4b37fad891b55d1ba
Anton Patsev <10828883+patsevanton@users.noreply.github.com>
2021-10-22 13:09:15 +0600

    Refactored createRegistryCreds.sh

--- scripts/createRegistryCreds.sh
+++ scripts/createRegistryCreds.sh
@@ -1,3 +1,5 @@
 # This will create a regcreds file to apply to the spring-petclinic namespace 
-
-kubectl create secret docker-registry regcred -n spring-petclinic --docker-server=<REGISTRY> --docker-username=<USERNAME> --docker-password=<PASSWORD> --docker-email=<EMAIL_ADDRESS> --dry-run=client -o yaml > ./k8s/init-namespace/02-regcreds.yaml
+echo "Before run script:"
+echo "export REPOSITORY_PREFIX=you-docker-account"
+echo "export export PASSWORD=you-password-docker-account"
+kubectl create secret docker-registry regcred -n spring-petclinic --docker-server="https://index.docker.io/v1/" --docker-username="$REPOSITORY_PREFIX" --docker-password="$PASSWORD" --docker-email="example@example.com" --dry-run=client -o yaml > ./k8s/init-namespace/02-regcreds.yaml
