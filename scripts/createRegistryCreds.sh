aec48283a647e27496b70f93e66470572a0fc834
Anton Patsev <10828883+patsevanton@users.noreply.github.com>
2021-10-22 14:40:29 +0600

    Refdactored createRegistryCreds.sh

--- scripts/createRegistryCreds.sh
+++ scripts/createRegistryCreds.sh
@@ -1,6 +1,15 @@
-# This will create a regcreds file to apply to the spring-petclinic namespace 
+# This will create a regcreds file to apply to the spring-petclinic namespace
 echo "Before run script:"
-echo "export REPOSITORY_PREFIX=you-docker-account"
-echo "export export PASSWORD=you-password-docker-account"
-echo "Use https://index.docker.io/v1/ as docker-server"
-kubectl create secret docker-registry regcred -n spring-petclinic --docker-server="https://index.docker.io/v1/" --docker-username="$REPOSITORY_PREFIX" --docker-password="$PASSWORD" --docker-email="example@example.com" --dry-run=client -o yaml > ./k8s/init-namespace/02-regcreds.yaml
+echo "Need export docker password. Example: export PASSWORD=you-password-docker-account"
+
+export REPOSITORY_PREFIX=harbor.myregistry.com/demo
+
+if [[ "$REPOSITORY_PREFIX" == *\/* ]]
+then
+  echo "Need export docker-username variable. Example: export USERNAME=you-docker-username"
+  kubectl create secret docker-registry regcred -n spring-petclinic --docker-server="$REPOSITORY_PREFIX" --docker-username="$USERNAME" --docker-password="$PASSWORD" --docker-email="example@example.com" --dry-run=client -o yaml > ./k8s/init-namespace/02-regcreds.yaml
+else
+  echo "Use $REPOSITORY_PREFIX as docker-username"
+  echo "Use https://index.docker.io/v1/ as docker-server"
+  kubectl create secret docker-registry regcred -n spring-petclinic --docker-server="https://index.docker.io/v1/" --docker-username="$REPOSITORY_PREFIX" --docker-password="$PASSWORD" --docker-email="example@example.com" --dry-run=client -o yaml > ./k8s/init-namespace/02-regcreds.yaml
+fi
