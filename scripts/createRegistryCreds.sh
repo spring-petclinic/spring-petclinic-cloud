16327e3e1f8a1f02dd17f7efbce430faf3f70c42
Anton Patsev <10828883+patsevanton@users.noreply.github.com>
2021-10-22 13:19:56 +0600

    Use https://index.docker.io/v1/ as docker-server in createRegistryCreds.sh

--- scripts/createRegistryCreds.sh
+++ scripts/createRegistryCreds.sh
@@ -2,4 +2,5 @@
 echo "Before run script:"
 echo "export REPOSITORY_PREFIX=you-docker-account"
 echo "export export PASSWORD=you-password-docker-account"
+echo "Use https://index.docker.io/v1/ as docker-server"
 kubectl create secret docker-registry regcred -n spring-petclinic --docker-server="https://index.docker.io/v1/" --docker-username="$REPOSITORY_PREFIX" --docker-password="$PASSWORD" --docker-email="example@example.com" --dry-run=client -o yaml > ./k8s/init-namespace/02-regcreds.yaml
